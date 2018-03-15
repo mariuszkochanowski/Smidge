d99fd45f20566710a334e27cc53fbeea078d86ed
Mariusz Kochanowski <kochanowskimariusz1@gmail.com>
2018-03-15 03:05:35 +0100

    .

---
+++ test/Smidge.Web.Test.Lib/AssemblyTools.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Smidge.Web.Test.Lib
+{
+    public class AssemblyTools
+    {
+        public static Assembly GetCurrentAssembly()
+        {
+            return typeof(AssemblyTools).Assembly;
+        }
+    }
+}
